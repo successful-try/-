bee5379f1379d3c3e89389c587bd1861a721086b
successful-try <85287269+successful-try@users.noreply.github.com>
2021-10-10 15:25:21 +0300

    Update E-carNumbers.cpp

--- Algorithm Trainings from Yandex (September 2021)/Sets/E-carNumbers.cpp
+++ Algorithm Trainings from Yandex (September 2021)/Sets/E-carNumbers.cpp
@@ -1,72 +1,66 @@
 #include <iostream>
 #include <set>
-#include <algorithm>
 #include <sstream>
 #include <string>
-#include <array>
+#include <memory>
+#include <algorithm>
+#include <unordered_map>
 #include <vector>
+#include <map>
+
 using namespace std;
 
-int main()
-{
-    string str;
+int main() {
+    int N, M;
+    string input;
 
-    int M;
-    cin >> M;
-
-    vector<string> Witness(M);
-
-    for(int i = 0; i < M; ++i){
-
-        cin >> str;
-        Witness[i] = str;
-
-    }
-
-    int N;
-    cin >> N;
-
-    vector<string> Sign(N);
-
-    for(int i = 0; i < N; ++i){
-
-        cin >> str;
-        Sign[i] = str;
-
-    }
-
-    vector<int> result;
-    int min_counter = 0;
-    string string_intersection,tmp_Sign, tmp_Witness ;
-
-    for(int i = 0; i < N; ++i){
-        min_counter = 20;
-        for(int j = 0; j < M; ++j){
-            tmp_Sign = Sign[i];
-            tmp_Witness = Witness[j];
-            sort(tmp_Sign.begin(), tmp_Sign.end());
-            sort(tmp_Witness.begin(),tmp_Witness.end());
-            string_intersection.clear();
-            set_intersection(tmp_Sign.begin(), tmp_Sign.end(),
-                             tmp_Witness.begin(), tmp_Witness.end(),
-                             inserter(string_intersection, string_intersection.begin()));
-            if(min_counter > string_intersection.size()){
-                min_counter = string_intersection.size();
+    getline(cin, input);
+    stringstream ss(input);
+    ss >> N;
+    map<int, set<char>> witnessNumbers, carNumbers; // key - input index, value - formatted number
+    unordered_map<int, string> cars; // key - input index, value - original number
+    for (int i = 0; i < N; ++i) {
+        getline(cin, input);
+        for (auto c: input) {
+            if (isupper(c) || isdigit(c)) {
+                witnessNumbers[i].insert(c);
             }
-
         }
-        result.push_back(min_counter);
     }
 
-   int max_ = *max_element(result.begin(),result.end());
-
-   for(int i = 0; i<N; ++i){
-        if(result[i] == max_){
-            cout << Sign[i] << endl;
+    getline(cin, input);
+    ss = stringstream(input);
+    ss >> M;
+    for (int i = 0; i < M; ++i) {
+        getline(cin, input);
+        for (auto c: input) {
+            if (isupper(c) || isdigit(c)) {
+                carNumbers[i].insert(c);
+            }
         }
-   }
+        cars[i] = move(input);
+    }
+
+    vector<string> answer;
+    int currentMaxQuantity = 0;
+    for (const auto&[carIndex, carNumber]: carNumbers) {
+        int quantity = 0;
+        for (const auto&[witnessIndex, witnessNumber]: witnessNumbers) {
+            if (includes(carNumber.begin(), carNumber.end(), witnessNumber.begin(), witnessNumber.end())) {
+                ++quantity;
+            }
+        }
+        if (quantity > currentMaxQuantity) {
+            currentMaxQuantity = quantity;
+            answer = vector<string>{cars[carIndex]};
+        } else if (quantity == currentMaxQuantity) {
+            answer.push_back(cars[carIndex]);
+        }
+    }
+
+    for (const auto& number: answer) {
+        cout << number << "\n";
+    }
 
     return 0;
 }
-
-
